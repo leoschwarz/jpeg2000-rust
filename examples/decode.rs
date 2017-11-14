94e3b5d249ed996ea459daeba43fc2482441a717
Leo Schwarz <mail@leoschwarz.com>
2017-11-14 23:34:12 +0100

    fix example

--- examples/decode.rs
+++ examples/decode.rs
@@ -3,11 +3,18 @@ extern crate image;
 
 use std::fs::File;
 use jpeg2000::decode::Codec;
+// TODO this shouldn't be needed here.
+use std::ffi::CString;
 
 fn main() {
-    let mut buffer = include_bytes!("./rust-logo-512x512-blk.jp2").to_vec();
+    // TODO this doesn't work as of now.
+    //let mut buffer = include_bytes!("./rust-logo-512x512-blk.jp2").to_vec();
+    //let img = jpeg2000::decode::load_from_memory(&mut buffer[..], Codec::JP2).unwrap();
 
-    let img = jpeg2000::decode::load_from_memory(&mut buffer[..], Codec::JP2).unwrap();
+    let img = jpeg2000::decode::load_from_file(
+        CString::new("./examples/rust-logo-512x512-blk.jp2").unwrap(),
+        Codec::JP2,
+    ).unwrap();
 
     let mut output = File::create("result.png").unwrap();
     img.save(&mut output, image::ImageFormat::PNG);
