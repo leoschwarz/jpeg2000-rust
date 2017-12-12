8f6fdd2d3385af8d0ab6c97bae7f12ad25e93cf3
Leo Schwarz <mail@leoschwarz.com>
2017-12-12 18:24:48 +0100

    Update example.

--- examples/decode.rs
+++ examples/decode.rs
@@ -1,22 +1,26 @@
-extern crate jpeg2000;
 extern crate image;
+extern crate jpeg2000;
 
 use std::fs::File;
-use jpeg2000::decode::Codec;
-// TODO this shouldn't be needed here.
-use std::ffi::CString;
+use jpeg2000::decode::{Codec, ColorSpace, DecodeConfig};
 
 fn main() {
-    // TODO this doesn't work as of now.
-    //let mut buffer = include_bytes!("./rust-logo-512x512-blk.jp2").to_vec();
-    //let img = jpeg2000::decode::load_from_memory(&mut buffer[..], Codec::JP2).unwrap();
+    let images = vec![
+        (include_bytes!("./images/rust_logo.jp2").to_vec(), "rust_logo", Codec::JP2),
+        (include_bytes!("./images/opensim_texture.jp2").to_vec(), "opensim_texture", Codec::J2K)
+    ];
 
-    let img = jpeg2000::decode::load_from_file(
-        CString::new("./examples/rust-logo-512x512-blk.jp2").unwrap(),
-        //CString::new("./examples/relax.jp2").unwrap(),
-        Codec::JP2,
-    ).unwrap();
+    for (mut data, basename, codec) in images {
+        let img = jpeg2000::decode::load_from_memory(
+            &mut data[..],
+            codec,
+            DecodeConfig {
+                default_colorspace: Some(ColorSpace::SRGB),
+                discard_level: 0,
+            },
+        ).unwrap();
 
-    let mut output = File::create("result.png").unwrap();
-    let _ = img.save(&mut output, image::ImageFormat::PNG);
+        let mut output = File::create(format!("{}.png", basename)).unwrap();
+        let _ = img.save(&mut output, image::ImageFormat::PNG);
+    }
 }
